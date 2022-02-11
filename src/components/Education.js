44d2c1c475c406ba08f6fb3963942cc3c9830649
Henry Derbyshire <henry@htderbyshire.com>
2022-02-11 13:23:26 +0000

    added onChange functions to education input fields

--- src/components/Education.js
+++ src/components/Education.js
@@ -12,11 +12,11 @@ class Education extends Component {
           <label htmlFor="school">School</label>
           <input type="text" id="school" onChange={(event) => this.props.handleChange(this.props.id, event, "school")}/>
           <label htmlFor="study">Topic</label>
-          <input type="text" id="study"/>
+          <input type="text" id="study" onChange={(event) => this.props.handleChange(this.props.id, event, "topic")}/>
           <label htmlFor="start-date">Start Date</label>
-          <input type="date" id="start-date"/>
+          <input type="date" id="start-date" onChange={(event) => this.props.handleChange(this.props.id, event, "startDate")}/>
           <label htmlFor="end-date">End Date</label>
-          <input type="date" id="end-date"/>
+          <input type="date" id="end-date" onChange={(event) => this.props.handleChange(this.props.id, event, "endDate")}/>
         </form>
       </div>
     )
